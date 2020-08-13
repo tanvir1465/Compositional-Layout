ec5b04cd0a2e73e61e8eca46a1fe86b8493d8eeb
Tanvir Hasan Piash <piash1465@gmail.com>
2020-08-14 02:05:19 +0600

    modified bundle extension for better error handling

--- CompositionalLayout/Controllers/AppsViewController.swift
+++ CompositionalLayout/Controllers/AppsViewController.swift
@@ -8,10 +8,32 @@
 
 import UIKit
 
+fileprivate typealias DataSource = UICollectionViewDiffableDataSource<Section, App>
+
 class AppsViewController: UIViewController {
+    
+    var collectionView: UICollectionView!
+    
+    let sections = Bundle.main.decode([Section].self, from: "dummyApps.json")
+    fileprivate var dataSource: DataSource?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Apps"
+        
+        addCollectionView()
+        registerCells()
+    }
+    
+    fileprivate func addCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+    }
+    
+    fileprivate func registerCells() {
+        
     }
     
 }
--- CompositionalLayout/Helpers/BundleExtension.swift
+++ CompositionalLayout/Helpers/BundleExtension.swift
@@ -10,11 +10,22 @@ import Foundation
 
 extension Bundle {
     
-    func decode<T: Decodable>(_ type: T.Type, from file: String) -> T {
-        guard let url = self.url(forResource: file, withExtension: nil) else { fatalError() }
-        guard let data = try? Data(contentsOf: url) else { fatalError() }
-        guard let result = try? JSONDecoder().decode(T.self, from: data) else { fatalError() }
-        return result
+    func decode<T: Decodable>(_ type: T.Type, from file: String) ->T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("The file you are trying to decode is unavailable")
+        }
+        do {
+            let data = try Data(contentsOf: url)
+            do {
+                let result = try JSONDecoder().decode(T.self, from: data)
+                return result
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        fatalError("Something bad hapenned")
     }
     
 }
