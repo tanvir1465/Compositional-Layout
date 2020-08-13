599315a857928ef43fd6bdc7a22bb07d050e8d2f
Tanvir Hasan Piash <piash1465@gmail.com>
2020-08-13 23:28:18 +0600

    removed storyboard for programatic approach

---
+++ CompositionalLayout/Controllers/AppsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AppsViewController.swift
+//  CompositionalLayout
+//
+//  Created by Tanvir Hasan Piash on 13/8/20.
+//  Copyright © 2020 Tanvir Hasan Piash. All rights reserved.
+//
+
+import UIKit
+
+class AppsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
